779364ec883600aff9212245504e75451116fcb1
Agathawei070 <agatha.wei@gmail.com>
2024-04-24 10:32:18 -0300

    desafio3

--- desafio3/app.py
+++ desafio3/app.py
@@ -6,10 +6,14 @@ app = Flask("__name__")
 def index():
     return render_template ("index.html")
 
-@app.route("/quemsomos")
-def quemsomos():
-    return render_template ("quemsomos.html")
+@app.route("/portifolio")
+def portifolio():
+    return render_template ("portifolio.html")
 
 @app.route("/contatos")
 def contatos():
     return render_template ("contatos.html")
+
+@app.route("/curriculo")
+def curriculo():
+    return render_template ("curriculo.html")
